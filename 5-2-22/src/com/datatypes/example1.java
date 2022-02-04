5fe24e6280bbae5ed7791daf3719ad6686374e4b
bharg <bharg@LAPTOP-IHHQCLPG>
2022-02-04 11:49:24 +0530

    DATATYPES added into the project 5-2-22

---
+++ 5-2-22/src/com/datatypes/example1.java
@@ -0,0 +1,14 @@
+package com.datatypes;
+
+public class example1 {
+
+	public static void main(String[] args) {
+	int customerid=766;
+	String customerName="bhargavi";
+	String customerAddress="vijayawada";
+	System.out.println(customerid);
+	System.out.println(customerName);
+	System.out.println(customerAddress);
+	}
+
+}
